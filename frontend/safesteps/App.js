e5742114934321b47d2595b7225cfd4f7eb7a79c
KitYan20 <112986746+KitYan20@users.noreply.github.com>
2023-10-05 17:48:48 -0400

    Created a button common for the app

--- frontend/safesteps/App.js
+++ frontend/safesteps/App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,Button, Text, View,SafeAreaView,Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title= "Press Me"
+        color="#979797"
+        onPress={() => Alert.alert('Continue?')}
+        // disabled
+      />
       <StatusBar style="auto" />
     </View>
   );
