b80deaea518bf9bfa4c2eda7bd072fcea47f0661
KitYan20 <112986746+KitYan20@users.noreply.github.com>
2023-11-12 11:26:19 -0500

    Included eduroam ip

--- backend/safesteps/run.py
+++ backend/safesteps/run.py
@@ -4,4 +4,5 @@ import os
 
 # ipconfig getifaddr en0
 
-os.system("python manage.py runserver 192.168.1.37:8000")
+#os.system("python manage.py runserver 192.168.1.37:8000")
+os.system("python manage.py runserver 10.239.95.26:8000")#
